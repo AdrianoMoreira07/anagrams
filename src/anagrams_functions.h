367bc4aeb3d50d4ca974f4133d9fce331d3068cb
Adriano dos Santos Moreira <72458779+AdrianoMoreira07@users.noreply.github.com>
2021-02-02 15:16:29 +0000

    Added header guards

--- src/anagrams_functions.h
+++ src/anagrams_functions.h
@@ -5,9 +5,13 @@
  * @author Adriano dos Santos Moreira <adriano.moreira.07@ull.edu.es> 
  * @brief Header for anagrams_functions.cc
  */
+#ifndef ANAGRAMS_FUNCTIONS
+#define ANAGRAMS_FUNCTIONS
 
 #include <string>
 
 void Start(int argc, char* argv[]);
 bool IsAnagram(const std::string& word1, const std::string& word2);
 std::string Lowercase(const std::string& word);
+
+#endif
